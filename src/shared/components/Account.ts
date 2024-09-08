50526c0ddf20eebdd1538e5fe137c474e41ee603
Im-Beast <franik.mateusz@gmail.com>
2024-09-08 16:00:29 +0200

    feat: improve the way `Account` component looks

--- src/shared/components/Account.ts
+++ src/shared/components/Account.ts
@@ -2,11 +2,14 @@ import { crayon } from "@crayon/crayon";
 import { Style, StyleBlock } from "@tui/nice";
 
 import { createButton } from "@tui/tui/components";
+import { tui } from "@tui/tui";
 
 import { ObservableWalletCache, WalletAccountInfo } from "$utils/wallet_cache.ts";
 import { formatXMR } from "$utils/monero.ts";
 
 import { colors } from "../styles.ts";
+import { computed, MaybeSignal, signal } from "@tui/signals";
+import { cropStart } from "@tui/strings/crop_start";
 
 const AccountButton = createButton((() => {
   const base = new Style({
@@ -28,7 +31,14 @@ const AccountButton = createButton((() => {
   return { base, hover, active };
 })());
 
-export type AccountBlock = StyleBlock;
+export type AccountBlock = MaybeSignal<StyleBlock>;
+
+const sizeCalc = (width: number) => (width - Math.max(width * 0.1, 30) - 2) * 0.95 - 6;
+
+const width = signal(sizeCalc(Deno.consoleSize().columns));
+tui.addEventListener("resize", ({ columns }) => {
+  width.set(sizeCalc(columns));
+});
 
 export function Account(
   cache: ObservableWalletCache,
@@ -36,15 +46,21 @@ export function Account(
 ): AccountBlock {
   const obscuredAddress = address.slice(0, 3) + " ... " + address.slice(-4);
 
-  const block = AccountButton(
-    `#${id} ${label.padEnd(18, " ")} ${obscuredAddress.padEnd(15, " ")} ${formatXMR(balance)} XMR`,
-    {
+  const block = computed([width], (width) => {
+    const blockWidth = Math.floor(width / 3);
+    // TODO: Button that can hold custom block instead of just text
+    //       What is done now is basically a temporary hack
+    const start = cropStart(`#${id} ${label}`, blockWidth).padEnd(blockWidth, " ");
+    const middle = cropStart(obscuredAddress, blockWidth).padEnd(blockWidth, " ");
+    const end = cropStart(`${formatXMR(balance)} XMR`, blockWidth).padStart(blockWidth, " ");
+
+    return AccountButton(`${start}${middle}${end}`, {
       forceClass: () => cache.currentAccount === id ? "active" : undefined,
       onClick() {
         cache.currentAccount = id;
       },
-    },
-  );
+    });
+  });
 
   return block;
 }
