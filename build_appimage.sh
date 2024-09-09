98c2b59b2dde64a6e706dde52e25406a8172c526
Im-Beast <franik.mateusz@gmail.com>
2024-09-09 13:25:50 +0200

    ci: AppImage builds

---
+++ build_appimage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pushd appimage
+wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage
+chmod +x appimagetool-x86_64.AppImage
+
+cp -r ../bin/ ./monero-tui.AppDir/usr/
+cp -r ../lib/ ./monero-tui.AppDir/usr/
+
+ARCH=x86_64 ./appimagetool-x86_64.AppImage -n ./monero-tui.AppDir ../bin/monero-tui.AppImage
+popd
