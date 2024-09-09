1d6b97841d17dbb21f7a4dacc13648f83c9c0383
Im-Beast <franik.mateusz@gmail.com>
2024-09-09 15:02:46 +0200

    feat: prepare AppImage generation for other architectures

--- build_appimage.sh
+++ build_appimage.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+# BIN_NAME = monero-tui-x86_64-unknown-linux-gnu
+# ARCH = x86_64
+
 pushd appimage
-wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage
-chmod +x appimagetool-x86_64.AppImage
+wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$ARCH.AppImage -O appimagetool.AppImage
+chmod +x appimagetool.AppImage
 
-cp -r ../bin/ ./monero-tui.AppDir/usr/
-cp -r ../lib/ ./monero-tui.AppDir/usr/
+pushd monero-tui.AppDir
+mkdir -p ./usr/bin
+mkdir -p ./usr/lib
+popd
 
-ARCH=x86_64 ./appimagetool-x86_64.AppImage -n ./monero-tui.AppDir ../bin/monero-tui-x86_64-unknown-linux-gnu.AppImage
+cp ../bin/$BIN_NAME ./monero-tui.AppDir/usr/bin/monero_tui
+cp ../lib/* ./monero-tui.AppDir/usr/lib/
+
+./appimagetool.AppImage -n ./monero-tui.AppDir ../bin/$BIN_NAME.AppImage
+chmod +x ../bin/$BIN_NAME.AppImage
 popd
