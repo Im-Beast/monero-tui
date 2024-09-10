c29e2f5e93768ef99266f0c04568427764655831
Im-Beast <franik.mateusz@gmail.com>
2024-09-10 18:56:13 +0200

    ci(experimental-build): debugging CI fail

--- build_appimage.sh
+++ build_appimage.sh
@@ -2,6 +2,8 @@
 # BIN_NAME = monero-tui-x86_64-unknown-linux-gnu
 # ARCH = x86_64
 
+echo "Before:"
+tree
 pushd appimage
 wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$ARCH.AppImage -O appimagetool.AppImage
 chmod +x appimagetool.AppImage
@@ -11,10 +13,11 @@ mkdir -p ./usr/bin
 mkdir -p ./usr/lib
 popd
 
-tree
 cp ../bin/$BIN_NAME ./monero-tui.AppDir/usr/bin/monero-tui
 cp ../lib/* ./monero-tui.AppDir/usr/lib/
 
 ./appimagetool.AppImage -n ./monero-tui.AppDir ../bin/$BIN_NAME.AppImage
 chmod +x ../bin/$BIN_NAME.AppImage
 popd
+echo "After:"
+tree
