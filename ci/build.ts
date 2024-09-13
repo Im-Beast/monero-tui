445f6acf90b1a7cb22ab1db1e794778902a49d37
Im-Beast <franik.mateusz@gmail.com>
2024-09-13 18:14:08 +0200

    ci(experimental-build): build respective architectures on native runners

--- ci/build.ts
+++ ci/build.ts
@@ -1,5 +1,6 @@
 import { build$, CommandBuilder } from "jsr:@david/dax";
 
+const ARCH = Deno.env.get("ARCH")! as "x86_64" | "aarch64";
 const DEBUG = Deno.env.get("DEBUG") === "true";
 const COIN = Deno.env.get("COIN");
 const MONERO_C_TAG = Deno.env.get("MONERO_C_TAG");
@@ -19,18 +20,34 @@ const $ = build$({
     .stderr("inherit"),
 });
 
-const triplets = [
-  ["x86_64", "x86_64-linux-gnu", "x86_64-unknown-linux-gnu"],
-  ["aarch64", "aarch64-linux-gnu", "aarch64-unknown-linux-gnu"],
-  ["x86_64", "x86_64-apple-darwin11", "x86_64-apple-darwin"],
-  ["aarch64", "aarch64-apple-darwin11", "aarch64-apple-darwin"],
-  ["x86_64", "x86_64-w64-mingw32", "x86_64-pc-windows-msvc"],
-] as const;
+const archTriplets = {
+  x86_64: [
+    ["x86_64-linux-gnu", "x86_64-unknown-linux-gnu"],
+    ["x86_64-apple-darwin11", "x86_64-apple-darwin"],
+    ["x86_64-w64-mingw32", "x86_64-pc-windows-msvc"],
+  ],
+  aarch64: [
+    ["aarch64-linux-gnu", "aarch64-unknown-linux-gnu"],
+    ["aarch64-apple-darwin11", "aarch64-apple-darwin"],
+  ],
+} as const;
+
+if (!(ARCH in archTriplets)) {
+  console.error(
+    "Invalid \x1b[1mARCH\x1b[0m env var:",
+    ARCH,
+    "Expected any of:",
+    Object.keys(archTriplets),
+  );
+  Deno.exit(1);
+}
+
+const triplets = archTriplets[ARCH];
 
 await Deno.remove("./build", { recursive: true }).catch(() => {});
 await Deno.mkdir("./build");
 
-for (const [arch, monerocTarget, denoTarget] of triplets) {
+for (const [monerocTarget, denoTarget] of triplets) {
   await Deno.remove("./lib", { recursive: true }).catch(() => {});
   await Deno.remove("./bin", { recursive: true }).catch(() => {});
   await Deno.mkdir("./lib");
@@ -49,7 +66,7 @@ for (const [arch, monerocTarget, denoTarget] of triplets) {
   // Building AppImages
   if (monerocTarget.includes("linux")) {
     let platform: string;
-    switch (arch) {
+    switch (ARCH) {
       case "x86_64":
         platform = "linux/amd64";
         break;
@@ -59,7 +76,7 @@ for (const [arch, monerocTarget, denoTarget] of triplets) {
     }
 
     await $.raw`docker run \
-            -e ARCH=${arch}  \
+            -e ARCH=${ARCH}  \
             -e BIN_NAME=${binaryName} \
             -v "$PWD":"$PWD":rw \
             -w "$PWD" \
